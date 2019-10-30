ed9e2f02bf5bb0f13d089d744fd61ab9f311ce26
Logan Karnes <logan.karnes98@gmail.com>
2019-10-29 18:53:07 -0700

    MVP complete

--- index.js
+++ index.js
@@ -9,48 +9,65 @@ server.use(express.json());
 
 server.post('/api/users', (req, res)=>{
     const data = req.body;
+
+    if(!data.name || !data.bio){
+        res.status(400).json({error: 'Please provide name and full bio for the user.'})
+    }
+
     db.insert(data)
     .then(user => {
-        res.json(user)
+        res.status(201).json(user)
     })
     .catch(err => {
-        res.send({message: 'error adding user'})
+        res.status(400).json({error: 'this is an error'})
     })
 })
 server.get('/api/users', (req, res)=>{
     db.find().then(users=> {
-        res.send(users)
+        res.status(200).json(users)
     })
     .catch(err => {
-        res.send({message: 'error getting users'});
+        res.status(500).json({error: 'user info could not be retrieved'});
     })
 })
 server.get('/api/users/:id', (req, res)=>{
     const id = req.params.id;
-    db.findById(id).then(user=> {
-        res.send(user)
+        db.findById(id).then(user=> {
+            if(!user){
+                res.status(404).json({ message: "The user with the specified ID does not exist." })
+            }else
+            res.status(200).json(user)
     })
     .catch(err=>{
-        res.send({message: 'error getting user'})
+        res.status(500).json({ message: "The user information could not be retrieved." })
     })
 })
 server.delete('/api/users/:id', (req, res)=>{
     const id = req.params.id;
     db.remove(id).then(user => {
-        res.json(user)
+        if(!user){
+            res.status(404).json({ message: "The user with the specified ID does not exist." })
+        }else
+        res.status(200).json(user)
     })
     .catch(err => {
-        res.send({message: 'error deleting user'})
+        res.json({message: 'The user could not be removed'})
     })
 })
 server.put('/api/users/:id', (req, res)=>{
     const id = req.params.id;
     const changes = req.body;
+    if(!changes.name || !changes.bio){
+        res.status(400).json({message: "Please provide name and bio for the user."})
+    }
     db.update(id, changes).then(user=> {
-        res.json(user)
+        if(!user){
+            res.status(404).json({message: "The user with the specified ID does not exist."})
+        }else
+        res.status(200).json(user)
     })
     .catch(err=> {
-        res.send({message: 'error updating user'})
+        res.status(400).json({message: "Please provide name and bio for the user."})    
     })
 })
 
